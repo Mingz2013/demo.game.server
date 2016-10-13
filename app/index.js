7a44f41d117c758a353aaa7894c616a017af43ac
mingz <305603665@qq.com>
2016-10-13 18:25:51 +0800

    init

--- app/index.js
+++ app/index.js
@@ -1,4 +1,10 @@
 var socketio = require('./socketio');
+exports.App = function () {
+    return {
+        run: function () {
+            var sioServer = new socketio.sioServer();
+            sioServer.bootStarp(8888, 'game');
+        }
+    }
+};
 
-var sioServer = new socketio.sioServer();
-sioServer.bootStarp(8888, 'game');
---
+++ setup.js
@@ -0,0 +1,7 @@
+/**
+ * Created by zhaojm on 13/10/2016.
+ */
+var app = require("./app/index").App();
+
+
+app.run();
